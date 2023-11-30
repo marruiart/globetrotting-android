e5d237ca0555a51eb4b6eb81d7f674d61b547b52
marruiart <marruiart@gmail.com>
2023-11-30 11:57:47 +0100

    Add nav bottom menu

--- app/src/main/java/com/marina/ruiz/globetrotting/ui/MainActivity.kt
+++ app/src/main/java/com/marina/ruiz/globetrotting/ui/MainActivity.kt
@@ -2,15 +2,33 @@ package com.marina.ruiz.globetrotting.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.marina.ruiz.globetrotting.R
 import com.marina.ruiz.globetrotting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_main_area) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.bookingCreationFormFragment,
+                R.id.characterListFragment
+            )
+        )
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/marina/ruiz/globetrotting/ui/fragments/BookingCreationFormFragment.kt
+++ app/src/main/java/com/marina/ruiz/globetrotting/ui/fragments/BookingCreationFormFragment.kt
@@ -1,33 +1,31 @@
 package com.marina.ruiz.globetrotting.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.marina.ruiz.globetrotting.databinding.FragmentBookingCreationFormBinding
 import com.marina.ruiz.globetrotting.databinding.FragmentCharacterDetailBinding
 
-class CharacterDetailFragment : Fragment() {
-    private lateinit var binding: FragmentCharacterDetailBinding
-    private val args: CharacterDetailFragmentArgs by navArgs()
+class BookingCreationFormFragment : Fragment() {
+    private lateinit var binding: FragmentBookingCreationFormBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCharacterDetailBinding
+        binding = FragmentBookingCreationFormBinding
             .inflate(inflater, container, false)
+        Log.d("FORM", "wiii")
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.charName.text = args.traveler.name
-        binding.charImg.load(args.traveler.image)
-        binding.charSpecies.text = args.traveler.species
-        binding.charGender.text = args.traveler.gender
     }
 }

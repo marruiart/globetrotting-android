a09ac1e1d9dab816fdcc3b4ff13b9b8c0118a1e7
marruiart <marruiart@gmail.com>
2023-12-16 13:19:09 +0100

    Bookings recyclerview working (not filled)

--- app/src/main/java/com/marina/ruiz/globetrotting/ui/adapter/BookingsListAdapter.kt
+++ app/src/main/java/com/marina/ruiz/globetrotting/ui/adapter/BookingsListAdapter.kt
@@ -1,20 +1,33 @@
 package com.marina.ruiz.globetrotting.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.marina.ruiz.globetrotting.R
 import com.marina.ruiz.globetrotting.data.repository.model.Booking
 import com.marina.ruiz.globetrotting.databinding.BookingItemBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class BookingsListAdapter() :
     ListAdapter<Booking, BookingsListAdapter.BookingViewHolder>(BookingDiffCallBack()) {
 
-    inner class BookingViewHolder(private val binding: BookingItemBinding) :
+    inner class BookingViewHolder(
+        private val binding: BookingItemBinding,
+        private val context: Context
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun bindBooking(booking: Booking) {
-
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val departureDate = dateFormat.format(Date(booking.departureDate))
+            val arrivalDate = dateFormat.format(Date(booking.arrivalDate))
+            binding.departureDate.text =
+                context.getString(R.string.booking_item_departure, departureDate)
+            binding.arrivalDate.text = context.getString(R.string.booking_item_arrival, arrivalDate)
         }
     }
 
@@ -29,7 +42,7 @@ class BookingsListAdapter() :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookingViewHolder {
         val binding = BookingItemBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
-        return BookingViewHolder(binding)
+        return BookingViewHolder(binding, parent.context)
     }
 
     override fun onBindViewHolder(holder: BookingViewHolder, position: Int) {

7169e9c32af752cde7b10873c8ece6569797555f
marruiart <marruiart@gmail.com>
2024-03-16 21:28:18 +0100

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,33 +44,36 @@ android {
 }
 
 dependencies {
+    val navVersion = "2.7.7"
+    val retrofitVersion = "2.9.0"
+    val roomVersion = "2.6.1"
+    val hiltVersion = "2.48.1"
+    val glideVersion = "4.15.1"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.annotation:annotation:1.6.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // FRAGMENTS
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+
     // NAVIGATION
-    val navVersion = "2.7.7"
     implementation("androidx.navigation:navigation-fragment-ktx:${navVersion}")
     implementation("androidx.navigation:navigation-ui-ktx:${navVersion}")
 
     // RETROFIT
-    val retrofitVersion = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:${retrofitVersion}")
     implementation("com.squareup.retrofit2:converter-gson:${retrofitVersion}")
 
     // ROOM
-    val roomVersion = "2.6.1"
     kapt("androidx.room:room-compiler:${roomVersion}")
     implementation("androidx.room:room-runtime:${roomVersion}")
     // para que funcione con observables
@@ -80,12 +83,10 @@ dependencies {
     implementation("io.coil-kt:coil:2.5.0")
 
     // HILT
-    val hiltVersion = "2.48.1"
     kapt("com.google.dagger:hilt-compiler:${hiltVersion}")
     implementation("com.google.dagger:hilt-android:${hiltVersion}")
 
     // GLIDE
-    val glideVersion = "4.15.1"
     implementation("com.github.bumptech.glide:glide:${glideVersion}")
     annotationProcessor("com.github.bumptech.glide:compiler:${glideVersion}")
 
@@ -96,4 +97,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-database")
 
+    // LOTTIES
+/*    val lottieVersion = "6.4.0"
+    implementation("om.airbnb.android:lottie:$lottieVersion")*/
 }

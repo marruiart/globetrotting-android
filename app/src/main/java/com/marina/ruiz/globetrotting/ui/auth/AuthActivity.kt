f3970f2059e419e7428cd9be12bf2ede5ebc559e
marruiart <marruiart@gmail.com>
2024-03-24 19:41:48 +0100

    Redesign login and register views

--- app/src/main/java/com/marina/ruiz/globetrotting/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/marina/ruiz/globetrotting/ui/auth/AuthActivity.kt
@@ -1,7 +1,10 @@
 package com.marina.ruiz.globetrotting.ui.auth
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.marina.ruiz.globetrotting.R
 import com.marina.ruiz.globetrotting.databinding.ActivityAuthBinding
@@ -13,9 +16,14 @@ class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_auth)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         supportFragmentManager.findFragmentById(R.id.fragment_auth_area) as NavHostFragment
     }
 }
--- app/src/main/java/com/marina/ruiz/globetrotting/ui/auth/AuthViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.marina.ruiz.globetrotting.ui.auth
-
-data class AuthViewState(
-    val isLoading: Boolean = false,
-    val isValidEmail: Boolean = true,
-    val isValidPassword: Boolean = true
-)

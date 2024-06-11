314d6e41d495c7a995eb5675cbcda337de9be520
marruiart <marina.ruiz.dev@gmail.com>
2024-06-11 09:41:33 +0200

    Fix filter name only by start characters

--- app/src/main/java/com/marina/ruiz/globetrotting/data/local/destination/DestinationDao.kt
+++ app/src/main/java/com/marina/ruiz/globetrotting/data/local/destination/DestinationDao.kt
@@ -19,7 +19,7 @@ interface DestinationDao {
         "SELECT d.id AS id, d.name AS name, d.type AS type, d.country AS country, d.continent AS continent, d.keywords AS keywords, d.price AS price, " +
                 "d.shortDescription AS shortDescription, d.description AS description, f.id AS favId " +
                 "FROM destination d LEFT JOIN favorite AS f ON f.destinationId = d.id " +
-                "WHERE d.name LIKE '%' || :searchQuery || '%' " +
+                "WHERE d.name LIKE :searchQuery || '%' " +
                 "ORDER BY d.name "
     )
     fun getAllDestinationsWithFavorites(searchQuery: String = ""): Flow<List<DestinationFavoritesEntity>>
@@ -28,7 +28,7 @@ interface DestinationDao {
         "SELECT d.id AS id, d.name AS name, d.type AS type, d.country AS country, d.continent AS continent, d.keywords AS keywords, d.price AS price, " +
                 "d.shortDescription AS shortDescription, d.description AS description, f.id AS favId " +
                 "FROM destination d LEFT JOIN favorite AS f ON f.destinationId = d.id " +
-                "WHERE favId IS NOT NULL AND d.name LIKE '%' || :searchQuery || '%' " +
+                "WHERE favId IS NOT NULL AND d.name LIKE :searchQuery || '%' " +
                 "ORDER BY d.name"
     )
     fun getAllFavDestinations(searchQuery: String = ""): Flow<List<DestinationFavoritesEntity>>
